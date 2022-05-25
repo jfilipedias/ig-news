ef160177bf17fe41721192ae04776d8effe3a51d
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-25 10:32:50 -0300

    Creates the SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 
-import styles from './styles.module.scss';
+import { SignInButton } from '../SignInButton'
+
+import styles from './styles.module.scss'
 
 export const Header: React.FC = () => {
   return (
@@ -18,6 +20,8 @@ export const Header: React.FC = () => {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
