5c36ad267c45c041646b51ad8d20313ca5d39c39
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-25 11:00:31 -0300

    Adds content and style to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Head from 'next/head'
+
+import styles from './home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +10,24 @@ const Home: NextPage = () => {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9 month</span>
+          </p>
+        </section>
+
+        <Image
+          src="/images/avatar.svg"
+          alt="An image of a girl sitting in a chair with a laptop, a cup of coffee and books on a table coding a react app."
+          width={336}
+          height={521}
+        />
+      </main>
     </>
   )
 }
