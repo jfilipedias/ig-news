f96e832302f7c1b78a52e163a2faf8bd68e0f555
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-25 09:04:59 -0300

    Adds fonts the projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </>
   )
 }
 
