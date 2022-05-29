b169a8ef1f5c0d0ffc4942e7b6041dffaee4d767
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-29 15:25:36 -0300

    Adds a new user on the fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { query } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -12,5 +15,38 @@ export default NextAuth({
         },
       },
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      console.log({ user })
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('users_by_email'),
+                  query.Casefold(user.email as string)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email: user.email as string } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('users_by_email'),
+                query.Casefold(user.email as string)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
