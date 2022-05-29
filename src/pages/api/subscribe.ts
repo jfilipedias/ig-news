df107ab9c6991dcfc1af73be1ac0f641f40a1a74
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-29 18:40:18 -0300

    Fixes stripe customer creation

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,7 +1,19 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { query } from 'faunadb'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
+
+import { fauna } from '../../services/fauna'
 import { stripe } from '../../services/stripe'
+
+type User = {
+  ref: {
+    id: string
+  },
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
@@ -11,12 +23,36 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const session = await getSession({ req })
 
-  const stripeCustomer = await stripe.customers.create({
-    email: session?.user?.email as string,
-  })
+  const user = await fauna.query<User>(
+    query.Get(
+      query.Match(
+        query.Index('users_by_email'),
+        query.Casefold(session?.user?.email as string)
+      )
+    )
+  )
+
+  let customerId = user.data.stripe_customer_id
+
+  if (!customerId) {
+    const stripeCustomer = await stripe.customers.create({
+      email: session?.user?.email as string,
+    })
+
+    await fauna.query(
+      query.Update(
+        query.Ref(query.Collection('users'), user.ref.id),
+        {
+          data: { stripe_customer_id: stripeCustomer.id },
+        }
+      )
+    )
+
+    customerId = stripeCustomer.id
+  }
 
   const stripeCheckoutSession = await stripe.checkout.sessions.create({
-    customer: stripeCustomer.id,
+    customer: customerId,
     payment_method_types: ['card'],
     billing_address_collection: 'required',
     line_items: [
