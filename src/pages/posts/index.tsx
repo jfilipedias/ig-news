f60220dd01a836f1d3bce06389190fbd768a52ce
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-06-09 22:18:59 -0300

    Implements formating ans listing posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,23 @@
 import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import { RichText } from 'prismic-dom'
+
 import { getPrismicClient } from '../../services/prismic'
 
 import styles from './styles.module.scss'
 
-const Posts: NextPage = () => {
+type Post = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+const Posts: NextPage<PostProps> = ({ posts }: PostProps) => {
   return (
     <>
       <Head>
@@ -13,23 +26,13 @@ const Posts: NextPage = () => {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Next.JS - Novidades na versão 10 e atualização do blog para melhorar a performance</strong>
-            <p>Se você nos acompanhou nos últimos posts, já viu que criamos um blog com um contador de visitas usando o MongoDB e Next.js, depois adicionamos a funcionalidade de dark mode.</p>
-          </a>
-
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Next.JS - Novidades na versão 10 e atualização do blog para melhorar a performance</strong>
-            <p>Se você nos acompanhou nos últimos posts, já viu que criamos um blog com um contador de visitas usando o MongoDB e Next.js, depois adicionamos a funcionalidade de dark mode.</p>
-          </a>
-
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Next.JS - Novidades na versão 10 e atualização do blog para melhorar a performance</strong>
-            <p>Se você nos acompanhou nos últimos posts, já viu que criamos um blog com um contador de visitas usando o MongoDB e Next.js, depois adicionamos a funcionalidade de dark mode.</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -47,8 +50,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
 
