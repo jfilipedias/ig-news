d4e82b42b90e9293b314678f58276c4a5a99ae45
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-06-11 23:10:10 -0300

    implements the post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,13 @@
 import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import { RichText } from 'prismic-dom'
 
 import { getPrismicClient } from '../../services/prismic'
 
 import styles from './styles.module.scss'
 
-type Post = {
+export type Post = {
   slug: string
   title: string
   excerpt: string
@@ -27,11 +28,13 @@ const Posts: NextPage<PostProps> = ({ posts }: PostProps) => {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
